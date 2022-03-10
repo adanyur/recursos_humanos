ed3f2973f0676e43fa3fbc6db01346541b9f8a65
yur <adanyur@gmail.com>
2022-03-10 04:10:13 -0500

    mejora en el navbar

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// window.onscroll = function () {
+//   var y = window.scrollY;
+//   if (y > 900) {
+//     console.log("color");
+//   } else {
+//     console.log("transparencia");
+//   }
+// };
